538251c1c75eebee11aab4abb1836896cd622a29
Phodal Huang <h@phodal.com>
2023-03-30 23:10:53 +0800

    feat: enable for fitler all tests java

--- src/main/kotlin/org/unitmesh/processor/Main.kt
+++ src/main/kotlin/org/unitmesh/processor/Main.kt
@@ -24,10 +24,11 @@ class Runner : CliktCommand(help = "Action Runner") {
         val content = file.readText()
         val config = Yaml.default.decodeFromString(deserializer = PreProcessorConfig.serializer(), content)
 
+        logger.info("Start to Clone code from GitHub")
         config.scm.forEach {
             // path from GitHub repository name
             val path = it.repository.split("/").last()
-            val targetPath = "origindatasets" + File.separator + path
+            val targetPath = clonedPath(path)
             // if directory exits and contains .git, then skip
             if (File(targetPath).exists() && File(targetPath + File.separator + ".git").exists()) {
                 logger.info("Skip $targetPath")
@@ -40,8 +41,21 @@ class Runner : CliktCommand(help = "Action Runner") {
             gitCommandManager.shallowClone(it.repository, it.branch)
         }
 
+        logger.info("Start to Filter Test Cases")
+        // walkdir in `origindatasets` and filter test cases
+        File("origindatasets").walkTopDown().forEach {
+            // if a file ends with `Test.java` or `Tests.java`, then copy it to `datasets`
+            if (it.isFile && (it.name.endsWith("Test.java") || it.name.endsWith("Tests.java"))) {
+                val targetPath = "datasets" + File.separator + it.path.split(File.separator).last()
+                it.copyTo(File(targetPath), true)
+            }
+        }
+
+
         logger.info("Runner finished")
     }
+
+    private fun clonedPath(path: String) = "origindatasets" + File.separator + path
 
     companion object {
         val logger: Logger = org.slf4j.LoggerFactory.getLogger(Runner::class.java)

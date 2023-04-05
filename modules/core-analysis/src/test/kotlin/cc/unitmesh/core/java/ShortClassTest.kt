c50783f15279506b3f1975da880697c08716ac39
Phodal Huang <h@phodal.com>
2023-04-05 16:29:22 +0800

    test: update sample for tests

--- modules/core-analysis/src/test/kotlin/cc/unitmesh/core/java/ShortClassTest.kt
+++ modules/core-analysis/src/test/kotlin/cc/unitmesh/core/java/ShortClassTest.kt
@@ -57,4 +57,97 @@ org.unitmesh.processor.TestClass(String, Int)
         """.trimIndent()
         assertEquals(expected, shortClass.toString())
     }
+
+    @Test
+    fun `test short class to string without fields`() {
+        val sourceCode = """public class PostService extends FormatDate {
+
+    private PostRepository postRepository;
+    private UserRepository userRepository;
+    private Set<Post> userPosts = new HashSet<>();
+    private ImageService imageService;
+
+    public PostService(PostRepository postRepository,UserRepository userRepository, ImageService imageService) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.imageService = imageService;
+    }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public Post addNewPost(Post post) {
+        //post.setCreatedBy("ADMIN");
+        Optional<User> userEmail =  userRepository.findByEmail("admin@gmail.com"); // CHANGE IT IN PRODUCTION
+        postRepository.save(post);
+        userPosts.add(post);
+        post.setUser(userEmail.get());
+        userRepository.save(userEmail.get());
+        return post;
+    }
+
+    public int saveImageToPost(String name, MultipartFile file, Post post) {
+        Post newPost = addNewPost(post);
+        try {
+            byte[] image = file.getBytes();
+            Image model = new Image(name, image);
+            int saveImage = imageService.saveImage(model);
+
+            if (saveImage == 1)
+            newPost.setImage(model);
+
+             newPost.setMyDate(formatDateToDayMonthYear());
+
+            byte[] encodingImage = Base64.getEncoder().encode(image);
+            String saveEncodedString = new String(encodingImage, "UTF-8");
+            newPost.getImage().setImageString(saveEncodedString);
+            postRepository.save(newPost);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+}
+
+"""
+        val toShortClass = JavaProcessor(sourceCode).toShortClass()
+        val expected = """
+    PostService(PostRepository, UserRepository, ImageService)
+    - fields: postRepository:PostRepository, userRepository:UserRepository, userPosts:Set<Post>, imageService:ImageService
+    - methods: findAll(): List<Post>, addNewPost(Post): Post, saveImageToPost(String, MultipartFile, Post): int
+        """.trimIndent()
+        assertEquals(expected, toShortClass.toString())
+    }
+
+    @Test
+    fun `test short class to string without fields and methods`() {
+        val sourceCode = """@Repository
+@Transactional
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+
+    Optional<Post> findById(Long id);
+
+    @Modifying
+    @Query("update Post p set p.postCommentsSize = :commentsSize where p.id = :id")
+    void updatePostCommentsSize(@Param("commentsSize") int postCommentsSize, @Param("id") Long id);
+
+
+    // find all post with releated topic
+
+    List<Post> findAllByPostTopics(String postTopic);
+
+    // and sort them date
+    List<Post> findDistinctByPostTopics(String postTopics);
+
+
+}"""
+        val toShortClass = JavaProcessor(sourceCode).toShortClass()
+        val expected = """PostRepository()
+- methods: findById(Long): Optional<Post>, updatePostCommentsSize(int, Long): void, findAllByPostTopics(String): List<Post>, findDistinctByPostTopics(String): List<Post>"""
+        assertEquals(expected, toShortClass.toString())
+    }
+
 }

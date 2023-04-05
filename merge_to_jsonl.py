6cc38c77c41bd55ef7fad79528d05d260a4866ff
Phodal Huang <h@phodal.com>
2023-04-05 14:23:11 +0800

    docs: limit json length

--- merge_to_jsonl.py
+++ merge_to_jsonl.py
@@ -1,13 +1,19 @@
 import os
 import json
+import sys
 
 
 def json_to_jsonl(source, target, ext='.json'):
+    index = 1
     walkdir = os.walk(source)
     with open(target, 'w') as out_file:
         for root, dirs, files in walkdir:
             for file in files:
                 if file.endswith(ext):
+                    if index == 10000:
+                        sys.exit(0)
+
+                    index += 1
                     # format json to one line
                     with open(os.path.join(root, file), 'r') as f:
                         data = json.load(f)

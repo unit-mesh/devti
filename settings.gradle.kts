29dd6dd5fbc7254673fa423d5d26a5d0a2bdb044
Phodal Huang <h@phodal.com>
2023-04-08 21:00:59 +0800

    refactor: rename modules to java modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,11 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 rootProject.name = "UnitProcessor"
 
 include(
-    ":modules:core-analysis",
+    ":java-modules:core-analysis",
 
-    ":modules:codegen-processor",
-    ":modules:swagger-processor",
-    ":modules:spring-processor",
-    ":modules:test-processor",
-    ":modules:requirements-diff-processor"
+    ":java-modules:codegen-processor",
+    ":java-modules:swagger-processor",
+    ":java-modules:spring-processor",
+    ":java-modules:test-processor",
+    ":java-modules:requirements-diff-processor"
 )

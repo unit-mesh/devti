f99e19505d1394e66d6f90d99987b6eb8cc63abc
Phodal Huang <h@phodal.com>
2023-04-09 13:55:00 +0800

    fix: fix compile error

--- kotlin-modules/unit-demo/build.gradle.kts
+++ kotlin-modules/unit-demo/build.gradle.kts
@@ -5,9 +5,9 @@ val exposed_version : String by project
 val h2_version : String by project
 
 plugins {
+    kotlin("jvm") version "1.8.20"
     id("io.ktor.plugin") version "2.2.4"
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.serialization)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
 }
 
 group = "unitmesh.cc"

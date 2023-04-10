058dfd013cb078cba78bb27fb92eed11cc67d8e4
Phodal Huang <h@phodal.com>
2023-04-10 17:43:16 +0800

    fix; fix support for java path issues

--- kotlin-modules/importer/src/main/kotlin/cc/unitmesh/importer/model/PackageUtil.kt
+++ kotlin-modules/importer/src/main/kotlin/cc/unitmesh/importer/model/PackageUtil.kt
@@ -1,9 +1,16 @@
 package cc.unitmesh.importer.model
 
 object PackageUtil {
+    /** input examples:
+     * 1. storage/src/main/kotlin/com/waz/zclient/storage/db/userclients/UserClientDao.kt
+     * 2. storage/src/main/java/com/waz/zclient/storage/db/userclients/UserClientDao.kt
+     * output:
+     * 1. com.waz.zclient.storage.db.userclients.UserClientDao
+     * 2. com.waz.zclient.storage.db.userclients.UserClientDao
+     **/
     fun pathToIdentifier(path: String): String {
-        val pathWithoutExtension = path.substringBeforeLast(".")
-        val pathWithoutSrc = pathWithoutExtension.substringAfterLast("src/main/kotlin/")
-        return pathWithoutSrc.replace("/", ".")
+        val regex = Regex("(kotlin|java)/(.+)\\.kt")
+        val match = regex.find(path)
+        return match?.groupValues?.get(2)?.replace('/', '.') ?: ""
     }
 }
--- kotlin-modules/importer/src/test/kotlin/cc/unitmesh/importer/model/PackageUtilTest.kt
+++ kotlin-modules/importer/src/test/kotlin/cc/unitmesh/importer/model/PackageUtilTest.kt
@@ -10,4 +10,11 @@ class PackageUtilTest {
         val identifier = PackageUtil.pathToIdentifier(path)
         assertEquals("com.waz.zclient.storage.db.userclients.UserClientDao", identifier)
     }
+
+    @Test
+    fun should_parser_java_path() {
+        val path = "storage/src/main/java/com/waz/zclient/storage/db/userclients/UserClientDao.kt"
+        val identifier = PackageUtil.pathToIdentifier(path)
+        assertEquals("com.waz.zclient.storage.db.userclients.UserClientDao", identifier)
+    }
 }

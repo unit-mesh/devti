ee3ce0ed02ab59b4e2b9310a9af4e73747324a07
Phodal Huang <h@phodal.com>
2023-04-12 09:30:37 +0800

    docs: init index

---
+++ index/build_index.py
@@ -0,0 +1,13 @@
+from llama_index import (
+    GPTKeywordTableIndex,
+    SimpleDirectoryReader,
+    LLMPredictor,
+    ServiceContext
+)
+from langchain import OpenAI
+
+documents = SimpleDirectoryReader('data').load_data()
+
+# define LLM
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)

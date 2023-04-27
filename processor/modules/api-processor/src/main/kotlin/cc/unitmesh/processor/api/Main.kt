7606d2ce09de87a42ea549fdac07f5bd19d14d03
Phodal Huang <h@phodal.com>
2023-04-27 10:38:24 +0800

    refactor: reorg code

--- processor/modules/api-processor/src/main/kotlin/cc/unitmesh/processor/api/Main.kt
+++ processor/modules/api-processor/src/main/kotlin/cc/unitmesh/processor/api/Main.kt
@@ -1,15 +1,22 @@
 package cc.unitmesh.processor.api
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
 import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-fun main(args: Array<String>) = Runner().main(args)
-class Runner : CliktCommand(help = "Action Runner") {
+fun main(args: Array<String>) = UnitConnector()
+    .subcommands(Prompting())
+    .main(args)
+
+val logger: Logger = LoggerFactory.getLogger(UnitConnector::class.java)
+
+class UnitConnector : CliktCommand() {
+    override fun run() = Unit
+}
+
+class Prompting : CliktCommand(help = "Prompting Runner") {
     override fun run() {
-        logger.info("Runner started")
-    }
-
-    companion object {
-        val logger: Logger = org.slf4j.LoggerFactory.getLogger(Runner::class.java)
+        logger.info("Prompting Started")
     }
 }

97df5f7a1f422441964ae14d48e97a3f4551fd61
Phodal Huang <h@phodal.com>
2023-04-27 15:46:14 +0800

    feat: enable parse from markdown

---
+++ processor/modules/api-processor/src/main/kotlin/cc/unitmesh/processor/api/parser/MarkdownParser.kt
@@ -0,0 +1,43 @@
+package cc.unitmesh.processor.api.parser
+
+import org.commonmark.Extension
+import org.commonmark.ext.gfm.tables.TableBlock
+import org.commonmark.ext.gfm.tables.TablesExtension
+import org.commonmark.node.AbstractVisitor
+import org.commonmark.node.CustomBlock
+import org.commonmark.node.FencedCodeBlock
+import org.commonmark.node.IndentedCodeBlock
+import org.commonmark.node.Node
+import org.commonmark.parser.Parser
+
+fun parseCodeFromString(markdown: String): List<String> {
+    val parser: Parser = Parser.builder().build()
+    val node: Node = parser.parse(markdown)
+    val visitor = CodeVisitor()
+    node.accept(visitor)
+
+    if (visitor.code.isEmpty()) {
+        val isJavaMethod = markdown.contains("public ") || markdown.contains("private ") || markdown.contains("protected ")
+        if (isJavaMethod) {
+            return listOf(markdown)
+        }
+
+        return listOf(markdown)
+    }
+
+    return visitor.code
+}
+
+internal class CodeVisitor : AbstractVisitor() {
+    var code = listOf<String>()
+
+    override fun visit(fencedCodeBlock: FencedCodeBlock?) {
+        if (fencedCodeBlock?.literal != null) {
+            this.code += fencedCodeBlock.literal
+        }
+    }
+
+    override fun visit(indentedCodeBlock: IndentedCodeBlock?) {
+        super.visit(indentedCodeBlock)
+    }
+}

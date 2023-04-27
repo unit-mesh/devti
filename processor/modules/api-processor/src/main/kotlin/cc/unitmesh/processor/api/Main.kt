6050131f53c9b1cd38eb6bbfc4ddd1ac2db34410
Phodal Huang <h@phodal.com>
2023-04-27 11:30:37 +0800

    feat: init first banking samples

--- processor/modules/api-processor/src/main/kotlin/cc/unitmesh/processor/api/Main.kt
+++ processor/modules/api-processor/src/main/kotlin/cc/unitmesh/processor/api/Main.kt
@@ -2,6 +2,9 @@ package cc.unitmesh.processor.api
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.int
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -16,6 +19,11 @@ class UnitConnector : CliktCommand() {
 }
 
 class Prompting : CliktCommand(help = "Prompting Runner") {
+    private val source by argument().file()
+    private val itemsNum by argument().int()
+    private val prompt by argument().file()
+    private val outputDir by argument().file()
+
     override fun run() {
         logger.info("Prompting Started")
     }

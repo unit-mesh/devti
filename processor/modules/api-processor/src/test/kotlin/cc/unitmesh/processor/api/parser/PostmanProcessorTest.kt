27eb8a813ee4b593ac2be9d403445a8b6e5d927a
Phodal Huang <h@phodal.com>
2023-04-28 15:10:42 +0800

    feat: init processor

---
+++ processor/modules/api-processor/src/test/kotlin/cc/unitmesh/processor/api/parser/PostmanProcessorTest.kt
@@ -0,0 +1,16 @@
+package cc.unitmesh.processor.api.parser
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class PostmanProcessorTest {
+    @Test
+    fun should_convert_postman_collection_to_api_details() {
+        val file = File("src/test/resources/testsets/postman.json")
+        val processor = PostmanProcessor(file)
+        val apiDetails = processor.convertApi()
+
+        assertEquals(0, apiDetails.size)
+    }
+}

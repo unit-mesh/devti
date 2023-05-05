aee72e9c66e07c4f2f52b8d475e190f283bbca42
Phodal Huang <h@phodal.com>
2023-05-05 15:45:53 +0800

    docs: update generating

---
+++ processor/modules/api-processor/src/main/kotlin/cc/unitmesh/processor/api/Generating.kt
@@ -0,0 +1,127 @@
+package cc.unitmesh.processor.api
+
+import cc.unitmesh.core.Instruction
+import cc.unitmesh.core.model.ApiCollection
+import cc.unitmesh.processor.api.base.ApiProcessor
+import cc.unitmesh.processor.api.render.MarkdownTableRender
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.default
+import com.github.ajalt.clikt.parameters.arguments.help
+import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.int
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+
+class Generating : CliktCommand() {
+    private val inputDir by argument().file().help("Input directory").default(File("input"))
+    private val domain by argument().file().default(File("domains.csv"))
+    private val outputDir by argument().file().default(File("output"))
+    private val maxLength by argument().int().default(4092)
+
+    override fun run() {
+        val outputDir = File("output", "markdown")
+        if (!outputDir.exists()) {
+            outputDir.mkdirs()
+        }
+
+        val instructions: MutableList<Instruction> = mutableListOf()
+
+        val translation = getDomainTranslate(domain)
+
+        val apiNames = mutableSetOf<String>()
+
+        inputDir.walk().forEach { file ->
+            if (file.isFile) {
+                var processor: ApiProcessor? = null
+                try {
+                    processor = ApiProcessorDetector.detectApiProcessor(file, withPostman = true)
+                } catch (e: Exception) {
+                    logger.info("Failed to parse ${file.absolutePath}", e)
+                }
+
+                if (processor == null) {
+                    return@forEach
+                }
+                val parentName = file.parentFile.name
+
+                try {
+                    val collections = processor.convertApi()
+                    val render = MarkdownTableRender()
+
+                    // create for each api
+                    collections.forEach {
+                        val single = render.render(listOf(it))
+                        if (single.length < 128 || single.length >= maxLength) {
+                            logger.info("Skip ${file.absolutePath} because it's too short")
+                            return@forEach
+                        }
+
+                        val serviceName = domainName(translation, it)
+                        apiNames += serviceName
+
+                        instructions += Instruction(
+                            instruction = ONE_API_INSTRUCTION,
+                            input = serviceName,
+                            output = single,
+                        )
+
+                        instructions += Instruction(
+                            instruction = randomInstruction(serviceName),
+                            input = "",
+                            output = single,
+                        )
+                    }
+
+                    // todo: spike better way to group APIs
+                    val output = createForGroup(render, collections, file, instructions, translation)
+                    if (output != null) {
+                        val outputFile = File(outputDir, "$parentName-${file.nameWithoutExtension}.md")
+                        outputFile.writeText(output)
+                    }
+                } catch (e: Exception) {
+                    logger.error("Failed to parse ${file.absolutePath}", e)
+                }
+            }
+        }
+
+        // write apiNames to CSV
+        val csv = File(this.outputDir, "apiNames.csv")
+        csv.writeText(apiNames.joinToString("\n"))
+
+        // write to jsonl
+        val jsonl = File(this.outputDir, "instructions.jsonl")
+        jsonl.writeText(instructions.joinToString("\n") { Json.encodeToString(it) })
+    }
+
+    private fun createForGroup(
+        render: MarkdownTableRender,
+        collections: List<ApiCollection>,
+        file: File,
+        instructions: MutableList<Instruction>,
+        domainTranslation: MutableMap<String, String>,
+    ): String? {
+        val output = render.render(collections)
+        val maybeAGoodApi = 128
+
+        if (output.length > maybeAGoodApi) {
+            instructions += Instruction(
+                instruction = GROUP_API_INSTRUCTION,
+                input = collections.joinToString(", ") { domainName(domainTranslation, it) },
+                output = output,
+            )
+
+            return output
+        } else {
+            logger.info("Skip ${file.absolutePath} because it's too short")
+        }
+
+        return null
+    }
+
+    private fun domainName(
+        domainTranslation: MutableMap<String, String>,
+        it: ApiCollection,
+    ) = domainTranslation[it.name] ?: it.name
+}
--- processor/modules/api-processor/src/main/kotlin/cc/unitmesh/processor/api/Main.kt
+++ processor/modules/api-processor/src/main/kotlin/cc/unitmesh/processor/api/Main.kt
@@ -1,7 +1,6 @@
 package cc.unitmesh.processor.api
 
 import cc.unitmesh.core.Instruction
-import cc.unitmesh.core.model.ApiCollection
 import cc.unitmesh.core.prompter.OpenAiPrompter
 import cc.unitmesh.processor.api.base.ApiProcessor
 import cc.unitmesh.processor.api.render.MarkdownTableRender
@@ -37,8 +36,8 @@ class UnitApi : CliktCommand() {
     }
 }
 
-private val GROUP_API_INSTRUCTION = "帮我设计一组 API："
-private val ONE_API_INSTRUCTION = "帮我设计一个银行的 API:"
+val GROUP_API_INSTRUCTION = "帮我设计一组 API："
+val ONE_API_INSTRUCTION = "帮我设计一个银行的 API:"
 
 fun randomInstruction(serviceName: String): String {
     val instructions = listOf(
@@ -56,118 +55,6 @@ fun randomInstruction(serviceName: String): String {
 
     val random = Random.nextInt(0, instructions.size)
     return instructions[random]
-}
-
-class Generating : CliktCommand() {
-    private val inputDir by argument().file().help("Input directory").default(File("input"))
-    private val domain by argument().file().default(File("domains.csv"))
-    private val outputDir by argument().file().default(File("output"))
-    private val maxLength by argument().int().default(4092)
-
-    override fun run() {
-        val outputDir = File("output", "markdown")
-        if (!outputDir.exists()) {
-            outputDir.mkdirs()
-        }
-
-        val instructions: MutableList<Instruction> = mutableListOf()
-
-        val translation = getDomainTranslate(domain)
-
-        val apiNames = mutableSetOf<String>()
-
-        inputDir.walk().forEach { file ->
-            if (file.isFile) {
-                var processor: ApiProcessor? = null
-                try {
-                    processor = ApiProcessorDetector.detectApiProcessor(file, withPostman = true)
-                } catch (e: Exception) {
-                    logger.info("Failed to parse ${file.absolutePath}", e)
-                }
-
-                if (processor == null) {
-                    return@forEach
-                }
-                val parentName = file.parentFile.name
-
-                try {
-                    val collections = processor.convertApi()
-                    val render = MarkdownTableRender()
-
-                    // create for each api
-                    collections.forEach {
-                        val single = render.render(listOf(it))
-                        if (single.length < 128 || single.length >= maxLength) {
-                            logger.info("Skip ${file.absolutePath} because it's too short")
-                            return@forEach
-                        }
-
-                        val serviceName = domainName(translation, it)
-                        apiNames += serviceName
-
-                        instructions += Instruction(
-                            instruction = ONE_API_INSTRUCTION,
-                            input = serviceName,
-                            output = single,
-                        )
-
-                        instructions += Instruction(
-                            instruction = randomInstruction(serviceName),
-                            input = "",
-                            output = single,
-                        )
-                    }
-
-                    // todo: spike better way to group APIs
-                    val output = createForGroup(render, collections, file, instructions, translation)
-                    if (output != null) {
-                        val outputFile = File(outputDir, "$parentName-${file.nameWithoutExtension}.md")
-                        outputFile.writeText(output)
-                    }
-                } catch (e: Exception) {
-                    logger.error("Failed to parse ${file.absolutePath}", e)
-                }
-            }
-        }
-
-        // write apiNames to CSV
-        val csv = File(this.outputDir, "apiNames.csv")
-        csv.writeText(apiNames.joinToString("\n"))
-
-        // write to jsonl
-        val jsonl = File(this.outputDir, "instructions.jsonl")
-        jsonl.writeText(instructions.joinToString("\n") { Json.encodeToString(it) })
-    }
-
-    private fun createForGroup(
-        render: MarkdownTableRender,
-        collections: List<ApiCollection>,
-        file: File,
-        instructions: MutableList<Instruction>,
-        domainTranslation: MutableMap<String, String>,
-    ): String? {
-        val output = render.render(collections)
-        val maybeAGoodApi = 128
-
-        if (output.length > maybeAGoodApi) {
-            instructions += Instruction(
-                instruction = GROUP_API_INSTRUCTION,
-                input = collections.joinToString(", ") { domainName(domainTranslation, it) },
-                output = output,
-            )
-
-            return output
-        } else {
-            logger.info("Skip ${file.absolutePath} because it's too short")
-        }
-
-        return null
-    }
-
-    private fun domainName(
-        domainTranslation: MutableMap<String, String>,
-        it: ApiCollection,
-    ) = domainTranslation[it.name] ?: it.name
 }
 
 class Prompting : CliktCommand() {
@@ -338,6 +225,97 @@ class Prompting : CliktCommand() {
             serviceNameMapFile.appendText(serviceName)
             serviceNameMapFile.appendText("\n")
         }
+    }
+}
+
+class Modeling : CliktCommand() {
+    private val instructions by argument().file().help("Instructions JSONL").default(File("instructions.jsonl"))
+    private val prompt by argument().file().default(File("domain-prompt.txt"))
+    private val inputDir by argument().file().help("Input directory").default(File("input"))
+    private val outputDir by argument().file().default(File("output"))
+    private val maxLength by argument().int().default(4092)
+
+    override fun run() {
+        logger.info("loading dotenv")
+        val dotenv = Dotenv.load()
+        val proxy = dotenv.get("OPEN_AI_PROXY")
+        val key = dotenv.get("OPEN_AI_KEY")
+
+        logger.info("key: $key, proxy: $proxy")
+
+        val outputDir = File("output", "markdown")
+        if (!outputDir.exists()) {
+            outputDir.mkdirs()
+        }
+
+        val promptText = prompt.readText()
+        val instructions: MutableList<Instruction> = mutableListOf()
+
+        // prompt with new
+        val domainDir = File(outputDir.absolutePath, "apis")
+        domainDir.mkdirs()
+
+        val prompter = OpenAiPrompter(key, proxy)
+
+        inputDir.walk().forEachIndexed { index, file ->
+            if (file.isFile) {
+                var processor: ApiProcessor? = null
+                try {
+                    processor = ApiProcessorDetector.detectApiProcessor(file, withPostman = true)
+                } catch (e: Exception) {
+                    logger.info("Failed to parse ${file.absolutePath}", e)
+                }
+
+                if (processor == null) {
+                    return@forEachIndexed
+                }
+
+                try {
+                    val collections = processor.convertApi()
+                    val render = MarkdownTableRender()
+
+                    val outputFile = File(domainDir, file.name)
+
+                    if (outputFile.exists()) {
+                        return@forEachIndexed
+                    }
+
+                    collections.forEach {
+                        val single = render.render(listOf(it))
+                        if (single.length < 128 || single.length >= maxLength) {
+                            logger.info("Skip ${file.absolutePath} because it's too short")
+                            return@forEach
+                        }
+                        val newPrompt = promptText.replace("{code}", it.name)
+
+                        var output = ""
+                        try {
+                            output = prompter.prompt(newPrompt)
+                            outputFile.writeText(output)
+                        } catch (e: Exception) {
+                            Thread.sleep(1000)
+                            logger.error("Error sleeping", e)
+                        }
+
+                        if (output.isEmpty()) {
+                            return@forEach
+                        }
+
+                        instructions += Instruction(
+                            instruction = newPrompt,
+                            input = newPrompt,
+                            output = output,
+                        )
+                    }
+                } catch (e: Exception) {
+                    logger.error("Failed to parse ${file.absolutePath}", e)
+                }
+            }
+        }
+
+        // write to jsonl
+        val jsonl = File(this.outputDir, "domains-instructions.jsonl")
+        jsonl.writeText(instructions.joinToString("\n") { Json.encodeToString(it) })
     }
 }
 

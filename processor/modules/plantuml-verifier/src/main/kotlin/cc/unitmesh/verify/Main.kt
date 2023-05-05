9155052314c7c7032eccf897231d302a64933549
Phodal Huang <h@phodal.com>
2023-05-05 20:56:18 +0800

    feat: add verify for plantuml

--- processor/modules/plantuml-verifier/src/main/kotlin/cc/unitmesh/verify/Main.kt
+++ processor/modules/plantuml-verifier/src/main/kotlin/cc/unitmesh/verify/Main.kt
@@ -1,8 +1,12 @@
 package cc.unitmesh.verify
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.default
+import com.github.ajalt.clikt.parameters.types.file
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.File
 
 fun main(args: Array<String>) = PlantUmlVerifier()
     .main(args)
@@ -10,7 +14,18 @@ fun main(args: Array<String>) = PlantUmlVerifier()
 val logger: Logger = LoggerFactory.getLogger(PlantUmlVerifier::class.java)
 
 class PlantUmlVerifier : CliktCommand() {
+    private val sourceDir by argument().file().default(File("output", "domain"))
+
     override fun run() {
         logger.info("Unit Connector Started")
+        // walkdir in source dir
+        sourceDir.walkTopDown().forEach {
+            if (it.isFile && it.extension == "puml") {
+                val isCorrect = PlantUmlParser(it).isCorrect()
+                if (!isCorrect) {
+                    logger.error("failed to verify ${it.absolutePath}")
+                }
+            }
+        }
     }
 }
---
+++ processor/modules/plantuml-verifier/src/main/kotlin/cc/unitmesh/verify/PlantUmlParser.kt
@@ -0,0 +1,20 @@
+package cc.unitmesh.verify
+
+import net.sourceforge.plantuml.SourceStringReader
+import java.io.File
+
+
+class PlantUmlParser(private val file: File) {
+    fun isCorrect(): Boolean {
+        val source = file.readText()
+
+        try {
+            SourceStringReader(source)
+        } catch (e: Exception) {
+            return false
+        }
+
+        return true
+    }
+
+}

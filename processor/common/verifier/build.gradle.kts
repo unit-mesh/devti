ddb25394354734388fc7e34936f44ec1466f84e6
Phodal Huang <h@phodal.com>
2023-05-07 10:28:16 +0800

    feat: init for sql

--- processor/common/verifier/build.gradle.kts
+++ processor/common/verifier/build.gradle.kts
@@ -11,8 +11,15 @@ repositories {
 dependencies {
     implementation(libs.serialization.json)
 
+    // UML
     implementation("net.sourceforge.plantuml:plantuml:1.2023.6")
+
+    // SQL
     implementation("com.github.jsqlparser:jsqlparser:4.6")
+
+    // Markdown
+    implementation("org.commonmark:commonmark:0.21.0")
+    implementation("org.commonmark:commonmark-ext-gfm-tables:0.21.0")
 
     // Logging
     implementation(libs.logging.slf4j.api)

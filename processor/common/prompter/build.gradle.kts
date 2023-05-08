ca4e681b3bc526defa4bf0d7bcb7fb8306ae6f9f
Phodal Huang <h@phodal.com>
2023-05-08 09:34:17 +0800

    fix: remove unused deps

--- processor/common/prompter/build.gradle.kts
+++ processor/common/prompter/build.gradle.kts
@@ -1,9 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.shadow)
     alias(libs.plugins.serialization)
-    application
 }
 
 repositories {
@@ -11,8 +9,6 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.clikt)
-
     implementation(libs.serialization.json)
 
     // Logging
@@ -21,10 +17,6 @@ dependencies {
 
     implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
     implementation("com.theokanning.openai-gpt3-java:service:0.12.0")
-
-    implementation("org.commonmark:commonmark:0.21.0")
-    implementation("org.commonmark:commonmark-ext-gfm-tables:0.21.0")
-    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
@@ -38,22 +30,4 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
-}
-
-application {
-    mainClass.set("cc.unitmesh.processor.swagger.MainKt")
-}
-
-tasks {
-    shadowJar {
-        manifest {
-            attributes(Pair("Main-Class", "cc.unitmesh.processor.api.MainKt"))
-        }
-        // minimize()
-        dependencies {
-            exclude(dependency("org.junit.jupiter:.*:.*"))
-            exclude(dependency("org.junit:.*:.*"))
-            exclude(dependency("junit:.*:.*"))
-        }
-    }
 }
